69a4555793439bdad0c362fd9d1a89924b0ae421
cuongpv_mb <cuongpv@intern.vn>
2025-05-09 11:21:35 +0700

    Update SDSBuildText

---
+++ lib/shares/base/ui/sds_build_text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../../../core/const/src.dart';
+
+class SDSBuildText extends StatelessWidget {
+  final String text;
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextStyle? style;
+
+  const SDSBuildText(
+    this.text, {
+    super.key,
+    this.textAlign,
+    this.maxLines,
+    this.style,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: textAlign ?? TextAlign.start,
+      style: style ?? AppTextStyle.font14Re,
+      maxLines: maxLines,
+    );
+  }
+}
