19ccf9b5a23cb011bc99027877ec0f6f372ecd03
Hiep <macbookpro@MacBook-Pro-cua-Macbook.local>
2025-05-28 15:28:02 +0700

    fix onRefresh and onLoadmore

--- lib/modules/select_staff/ui/select_staff_widget.dart
+++ lib/modules/select_staff/ui/select_staff_widget.dart
@@ -21,7 +21,9 @@ extension SelectStaffWidget on SelectStaffPage {
         hintTextColor: AppColors.thumbColorSwitch,
         hintTextSize: AppDimens.fontSmall(),
         iconAssets: Assets.ASSETS_ICONS_IC_SEARCH_SVG,
-        onChanged: (_) => controller.functionSearch(),
+        onChanged: (_) {
+          controller.functionSearch();
+        },
         border: _buildOutlineBorder(),
         focusedBorder: _buildOutlineBorder(),
         enabledBorder: _buildOutlineBorder(),
@@ -40,53 +42,64 @@ extension SelectStaffWidget on SelectStaffPage {
   }
 
   Widget _buildItemStaff(SelectStaffResponse item) {
-    return Obx(() {
-      final isSelected = controller.staffSelected.value == item;
-      return InkWell(
-        onTap: () => controller.staffSelected.value = item,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SDSImageSvg(
-              Assets.ASSETS_ICONS_IC_PERSON_SVG,
-              height: AppDimens.sizeIcon32,
-              color: AppColors.primaryColor,
-            ),
-            sdsSBWidth12,
-            Expanded(child: _buildStaffInfo(item)),
-            Icon(
-              Icons.check_outlined,
-              color: isSelected
-                  ? AppColors.primaryColor
-                  : AppColors.colorTransparent,
-            ),
-          ],
-        ).paddingSymmetric(vertical: AppDimens.paddingSmallest),
-      );
-    });
-  }
-
-  Widget _buildStaffInfo(SelectStaffResponse item) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        RichText(
-          text: TextSpan(
+    return Obx(
+      () {
+        final isSelected = controller.staffSelected.value == item;
+        return InkWell(
+          onTap: () {
+            controller.staffSelected.value = item;
+          },
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              TextSpan(text: item.hoTen, style: AppTextStyle.font16Bo),
-              if (item.soCCCD.isNotEmpty)
-                TextSpan(
-                  text: ' (${item.soCCCD})',
-                  style: AppTextStyle.font14Re
-                      .copyWith(color: AppColors.colorBlack),
+              SDSImageSvg(
+                Assets.ASSETS_ICONS_IC_PERSON_SVG,
+                height: AppDimens.sizeIcon32,
+                color: AppColors.primaryColor,
+              ),
+              sdsSBWidth12,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: item.hoTen,
+                            style: AppTextStyle.font16Bo,
+                          ),
+                          if (item.soCCCD.isNotEmpty)
+                            TextSpan(
+                              text: ' (${item.soCCCD})',
+                              style: AppTextStyle.font14Re.copyWith(
+                                color: AppColors.colorBlack,
+                              ),
+                            ),
+                        ],
+                      ),
+                      maxLines: 2,
+                    ),
+                    Visibility(
+                      visible: item.chucVu.isNotEmpty,
+                      child: SDSBuildText(
+                        item.chucVu,
+                        style: AppTextStyle.font12Re,
+                      ),
+                    )
+                  ],
                 ),
+              ),
+              Icon(
+                Icons.check_outlined,
+                color: isSelected
+                    ? AppColors.primaryColor
+                    : AppColors.colorTransparent,
+              ),
             ],
-          ),
-          maxLines: 2,
-        ),
-        if (item.chucVu.isNotEmpty)
-          SDSBuildText(item.chucVu, style: AppTextStyle.font12Re),
-      ],
+          ).paddingSymmetric(vertical: AppDimens.paddingSmallest),
+        );
+      },
     );
   }
 
@@ -99,19 +112,17 @@ extension SelectStaffWidget on SelectStaffPage {
             onRefresh: controller.onRefresh,
             onLoadMore: controller.onLoadMore,
             enablePullUp: true,
-            child: Obx(
-              () => controller.listStaffSelect.isEmpty
-                  ? UtilWidget.buildEmptyList()
-                  : ListView.separated(
-                      itemBuilder: (context, index) {
-                        final item = controller.listStaffSelect[index];
-                        return _buildItemStaff(item);
-                      },
-                      itemCount: controller.listStaffSelect.length,
-                      separatorBuilder: (_, __) =>
-                          UtilWidget.buildDividerDefault(),
-                    ),
-            ),
+            child: controller.listStaffSelect.isEmpty
+                ? UtilWidget.buildEmptyList()
+                : ListView.separated(
+                    itemBuilder: (context, index) {
+                      final item = controller.listStaffSelect[index];
+                      return _buildItemStaff(item);
+                    },
+                    itemCount: controller.listStaffSelect.length,
+                    separatorBuilder: (context, index) =>
+                        UtilWidget.buildDividerDefault(),
+                  ),
           ),
         ).paddingAll(AppDimens.paddingSmall),
       ),
@@ -121,7 +132,9 @@ extension SelectStaffWidget on SelectStaffPage {
   Widget _buildButtonConfirm() {
     return UtilWidget.buildSolidButton(
       title: LocaleKeys.staffList_confirm.tr,
-      onPressed: () => Get.back(result: controller.staffSelected.value),
+      onPressed: () {
+        Get.back(result: controller.staffSelected.value);
+      },
     ).paddingSymmetric(vertical: AppDimens.defaultPadding);
   }
 }
