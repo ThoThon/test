a3f494d12349724ead6e2b12ea8de0a8e6b338e1
cuongpv_mb <cuongpv@intern.vn>
2025-06-05 17:37:14 +0700

    Fix timeout

--- lib/modules/declare/declaration_list/repository/declaration_list_repository.dart
+++ lib/modules/declare/declaration_list/repository/declaration_list_repository.dart
@@ -2,6 +2,8 @@ import 'package:v_bhxh/base_app/model/base_response.dart';
 import 'package:v_bhxh/base_app/repository_base/base_repository.dart';
 import 'package:v_bhxh/modules/src.dart';
 
+const _signDocumentTimeOut = Duration(minutes: 3);
+
 class DeclarationListRepository extends BaseRepository {
   DeclarationListRepository(super.controller);
 
@@ -14,7 +16,7 @@ class DeclarationListRepository extends BaseRepository {
         "kyKeKhaiId": declarationPeriodId,
       },
       // Cần chờ user mở app mysign để ký số nên set timeout là 3 phút
-      timeOut: const Duration(seconds: 10),
+      timeOut: _signDocumentTimeOut,
       functionError: (err) {
         logger.e(err);
         throw err;
