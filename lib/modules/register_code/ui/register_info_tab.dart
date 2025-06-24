b10d643cf99012bf6b27aceb852ec8c0449763a5
Trung Đàm Thành <trung1qaz2wsx3edc4rfv@gmail.com>
2025-06-24 08:50:12 +0700

    BHW-2468

--- lib/modules/register_code/ui/register_info_tab.dart
+++ lib/modules/register_code/ui/register_info_tab.dart
@@ -63,7 +63,7 @@ extension RegisterInfoTab on RegisterCodePage {
       funcSelect: (didChange) {
         Get.bottomSheet(
           BottomSheetSearch<SocialAgencyModel>(
-            title: LocaleKeys.unitInfo_selectSocialAgency.tr, // Chuyển từ "Dân tộc" thành "Chọn cơ quan BHXH"
+            title: LocaleKeys.unitInfo_selectSocialAgency.tr,
             maxLength: 20,
             listFilter: AppData.instance.socialAgency.toList(),
             selectedItem: controller.socialAgency.value,
@@ -95,7 +95,7 @@ extension RegisterInfoTab on RegisterCodePage {
       funcSelect: (didChange) {
         Get.bottomSheet(
           BottomSheetSearch<ProvinceModel>(
-            title: LocaleKeys.unitInfo_selectProvince.tr, // Chuyển từ "Dân tộc" thành "Chọn tỉnh/thành phố"
+            title: LocaleKeys.unitInfo_selectProvince.tr,
             maxLength: 20,
             listFilter: AppData.instance.provinces.toList(),
             selectedItem: controller.provinceReceive.value,
