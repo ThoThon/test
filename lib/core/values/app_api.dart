c88789d4a89c40d652d30941b3ed998d55235f87
cuongpv_mb <cuongpv@intern.vn>
2025-07-08 16:11:15 +0700

    BHW-2577

--- lib/core/values/app_api.dart
+++ lib/core/values/app_api.dart
@@ -93,6 +93,7 @@ class AppApi {
       '/api/HoSoTk1/delete-tk1-d01';
   static const String urlAddTk1 = '/api/HoSoTk1/add-tk1';
   static const String urlGetTk1Detail = '/api/HoSoTk1/get-detail';
+  static const String urlUpdateTk1 = '/api/HoSoTk1/update-tk1';
 
   // === Kết thúc các api liên quan đến thủ tục 607 ===
 }
