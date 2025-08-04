28232bb88a8087672a524d63df7072a05cd83590
cuongpv_mb <cuongpv@intern.vn>
2025-08-04 15:19:25 +0700

    Remove unused code

--- lib/modules/declare/declare_info/clean/presentation/controller/controller_src.dart
+++ lib/modules/declare/declare_info/clean/presentation/controller/controller_src.dart
@@ -1,3 +1,2 @@
 export 'select_district_controller.dart';
-export 'select_hospital_controller.dart';
 export 'select_ward_controller.dart';
--- lib/modules/declare/declare_info/clean/presentation/presentation_src.dart
+++ lib/modules/declare/declare_info/clean/presentation/presentation_src.dart
@@ -1,4 +1,3 @@
 export 'select_district_bts.dart';
-export 'select_hospital_bts.dart';
 export 'select_ward_bts.dart';
 export 'controller/controller_src.dart';
