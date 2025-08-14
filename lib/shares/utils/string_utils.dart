9a472d569b4b46951f7f97b26fcd2c04ea0eebc4
cuongpv_mb <cuongpv@intern.vn>
2025-08-14 12:01:29 +0700

    Add string util

--- lib/shares/utils/string_utils.dart
+++ lib/shares/utils/string_utils.dart
@@ -1,6 +1,13 @@
+import 'package:tiengviet/tiengviet.dart';
+
 extension StringExt on String {
   bool get isEmailValid {
     final emailRegex = RegExp(r'^[\w\-.+%]+@(?:[\w-]+\.)+[\w]{2,6}$');
     return emailRegex.hasMatch(this);
+  }
+
+  /// Kiểm tra xem chuỗi có chứa ký tự tiếng Việt hay không
+  bool get hasVietnameseChars {
+    return TiengViet.parse(this) != this;
   }
 }
