3722b1292042de03f7397df93bc4e77c1230d98d
cuongpv_mb <cuongpv@intern.vn>
2025-08-19 17:32:58 +0700

    Add build prod sds script

---
+++ .build/prod_sds/android.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+
+# Đường dẫn đến tệp tin txt chứa nội dung ghi chú
+note_file=".build/note.txt"
+
+# fvm flutter build apk --release --flavor prodsds --target lib/main_prod_sds.dart
+
+firebase appdistribution:distribute './build/app/outputs/flutter-apk/app-prodsds-release.apk'  \
+    --app 1:1048213822229:android:e23e7b76ae892dd47d671c  \
+    --release-notes-file "$note_file" \
+    --groups "android-testers"
+
+echo '-------------------'
+echo 'Build Android done'
