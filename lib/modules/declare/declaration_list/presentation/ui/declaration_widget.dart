570e0ba257e63176951673847b00f4cac1aaf401
hiephx <hiepthon10vandu@gmail.com>
2025-10-04 08:30:20 +0700

    Done

--- lib/modules/declare/declaration_list/presentation/ui/declaration_widget.dart
+++ lib/modules/declare/declaration_list/presentation/ui/declaration_widget.dart
@@ -2,29 +2,24 @@ part of 'declaration_list_page.dart';
 
 extension DeclarationListWidget on DeclarationListPage {
   Widget _buildBody() {
-    final saveResult = controller.argument.saveXmlResult;
-    final declareHistoryRecord =
-        controller.argument.declarationHistoryRecordList;
-    final isFromHistoryPage = controller.argument.isFromHistoryPage;
+    final args = controller.argument;
+    final saveResult = args.saveXmlResult;
+    final declareHistoryRecord = args.declarationHistoryRecordList;
+    final isFromHistoryPage = args.isFromHistoryPage;
 
     final hasDsM01hsb = isFromHistoryPage
         ? declareHistoryRecord?.hasDsM01hsb
         : saveResult?.hasDsM01hsb;
-
     final hasTsM01hsb = isFromHistoryPage
         ? declareHistoryRecord?.hasTsM01hsb
         : saveResult?.hasTsM01hsb;
-
     final hasOdM01hsb = isFromHistoryPage
         ? declareHistoryRecord?.hasOdM01hsb
         : saveResult?.hasOdM01hsb;
-
     final hasD02 =
         isFromHistoryPage ? declareHistoryRecord?.hasD02 : saveResult?.hasD02;
-
     final hasD01 =
         isFromHistoryPage ? declareHistoryRecord?.hasD01 : saveResult?.hasD01;
-
     final attachPreviewPath = isFromHistoryPage
         ? declareHistoryRecord?.attachPreviewPath
         : saveResult?.attachPreviewPath;
@@ -48,86 +43,80 @@ extension DeclarationListWidget on DeclarationListPage {
                   if (hasDsM01hsb ?? false)
                     _buildDeclarationItem(
                       title: LocaleKeys.declareInfo_healingHsbList.tr,
-                      onTap: () {
-                        controller.getPreviewPdf(
-                          id: declareHistoryRecord?.dsM01hsbId,
-                          title: LocaleKeys
-                              .declareInfo_participationDeclaration.tr,
-                          previewDocumentType:
-                              PreviewDocumentTypeEnum.healingHsb,
-                        );
-                      },
+                      onTap: () => controller.getPreviewPdf(
+                        id: declareHistoryRecord?.dsM01hsbId,
+                        title:
+                            LocaleKeys.declareInfo_participationDeclaration.tr,
+                        previewDocumentType: PreviewDocumentTypeEnum.healingHsb,
+                      ),
                     ).paddingOnly(bottom: AppDimens.paddingSmall),
                   if (hasTsM01hsb ?? false)
                     _buildDeclarationItem(
                       title: LocaleKeys.declareInfo_maternityHsbList.tr,
-                      onTap: () {
-                        controller.getPreviewPdf(
-                          id: declareHistoryRecord?.tsM01hsbId,
-                          title: LocaleKeys
-                              .declareInfo_participationDeclaration.tr,
-                          previewDocumentType:
-                              PreviewDocumentTypeEnum.maternityHsb,
-                        );
-                      },
+                      onTap: () => controller.getPreviewPdf(
+                        id: declareHistoryRecord?.tsM01hsbId,
+                        title:
+                            LocaleKeys.declareInfo_participationDeclaration.tr,
+                        previewDocumentType:
+                            PreviewDocumentTypeEnum.maternityHsb,
+                      ),
                     ).paddingOnly(bottom: AppDimens.paddingSmall),
                   if (hasOdM01hsb ?? false)
                     _buildDeclarationItem(
                       title: LocaleKeys.declareInfo_sickHsbList.tr,
-                      onTap: () {
-                        controller.getPreviewPdf(
-                          id: declareHistoryRecord?.odM01hsbId,
-                          title: LocaleKeys
-                              .declareInfo_participationDeclaration.tr,
-                          previewDocumentType: PreviewDocumentTypeEnum.sickHsb,
-                        );
-                      },
+                      onTap: () => controller.getPreviewPdf(
+                        id: declareHistoryRecord?.odM01hsbId,
+                        title:
+                            LocaleKeys.declareInfo_participationDeclaration.tr,
+                        previewDocumentType: PreviewDocumentTypeEnum.sickHsb,
+                      ),
                     ).paddingOnly(bottom: AppDimens.paddingSmall),
                   if (hasD02 ?? false)
                     _buildDeclarationItem(
                       title: LocaleKeys.declareInfo_d02LaborList.tr,
-                      onTap: () {
-                        controller.getPreviewPdf(
-                          id: declareHistoryRecord?.d02Id,
-                          title: LocaleKeys.declareInfo_laborList.tr,
-                          previewDocumentType: PreviewDocumentTypeEnum.d02,
-                          isRotateHorizontal: true,
-                        );
-                      },
+                      onTap: () => controller.getPreviewPdf(
+                        id: declareHistoryRecord?.d02Id,
+                        title: LocaleKeys.declareInfo_laborList.tr,
+                        previewDocumentType: PreviewDocumentTypeEnum.d02,
+                        isRotateHorizontal: true,
+                      ),
                     ).paddingOnly(bottom: AppDimens.paddingSmall),
-                  _buildTk1Preview(),
+                  isFromHistoryPage
+                      ? _buildTk1RecordPdf(
+                          declareHistoryRecord?.staffs ?? [],
+                          declareHistoryRecord?.tk1Id,
+                        )
+                      : _buildTk1PreviewPdf(saveResult?.tk1s ?? []),
                   if (hasD01 ?? false)
                     _buildDeclarationItem(
                       title: LocaleKeys.declareInfo_d01infoTable.tr,
-                      onTap: () {
-                        controller.getPreviewPdf(
-                          id: declareHistoryRecord?.d01Id,
-                          title: LocaleKeys
-                              .declareInfo_participationDeclaration.tr,
-                          previewDocumentType: PreviewDocumentTypeEnum.d01,
-                        );
-                      },
+                      onTap: () => controller.getPreviewPdf(
+                        id: declareHistoryRecord?.d01Id,
+                        title:
+                            LocaleKeys.declareInfo_participationDeclaration.tr,
+                        previewDocumentType: PreviewDocumentTypeEnum.d01,
+                      ),
                     ).paddingOnly(bottom: AppDimens.paddingSmall),
                   if (attachPreviewPath != null)
                     _buildDeclarationItem(
                       title: LocaleKeys.unitInfo_missingFileInclude.tr,
-                      onTap: () {
-                        Get.toNamed(
-                          AppRoutesCl.viewPdf.path,
-                          arguments: ViewPdfArgument(
-                            url: saveResult?.attachPreviewPath ??
-                                declareHistoryRecord?.attachPreviewPath ??
-                                '',
-                            title: LocaleKeys.unitInfo_missingFileInclude.tr,
-                          ),
-                        );
-                      },
+                      onTap: () => Get.toNamed(
+                        AppRoutesCl.viewPdf.path,
+                        arguments: ViewPdfArgument(
+                          url: saveResult?.attachPreviewPath ??
+                              declareHistoryRecord?.attachPreviewPath ??
+                              '',
+                          title: LocaleKeys.unitInfo_missingFileInclude.tr,
+                        ),
+                      ),
                     ),
                 ],
               ),
             ),
           ),
         ),
+
+        /// Bottom button
         UtilWidget.buildSolidButton(
           height: AppDimens.btnLargeFigma,
           borderRadius: AppDimens.radius30,
@@ -145,12 +134,46 @@ extension DeclarationListWidget on DeclarationListPage {
     ).paddingOnly(top: AppDimens.defaultPadding);
   }
 
-  Widget _buildTk1Preview() {
-    final paths = controller.argument.saveXmlResult?.tk1s;
-    if (paths == null) {
-      return UtilWidget.shrink;
-    }
+  Widget _buildTk1PreviewPdf(List<Tk1PreviewPath> tk1List) {
+    if (tk1List.isEmpty) return UtilWidget.shrink;
 
+    return _buildExpansionTile(
+      title: LocaleKeys.declareInfo_tk1Declaration.tr,
+      children: tk1List.map(
+        (path) => _buildOnTapTk1(
+          name: path.name,
+          onTap: () => controller.createPdf(
+            previewDocumentType: PreviewDocumentTypeEnum.tk1,
+            documentRecordId: path.documentRecordId,
+            title: LocaleKeys.declareInfo_participationDeclaration.tr,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTk1RecordPdf(List<StaffInfo> staffs, String? tk1Id) {
+    if (staffs.isEmpty) return UtilWidget.shrink;
+
+    return _buildExpansionTile(
+      title: LocaleKeys.declareInfo_tk1Declaration.tr,
+      children: staffs.map(
+        (path) => _buildOnTapTk1(
+          name: path.name,
+          onTap: () => controller.recordPdf(
+            title: LocaleKeys.declareInfo_participationDeclaration.tr,
+            staffId: path.id,
+            id: tk1Id,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildExpansionTile({
+    required String title,
+    required Iterable<Widget> children,
+  }) {
     return ExpansionTile(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(AppDimens.radius16),
@@ -167,46 +190,40 @@ extension DeclarationListWidget on DeclarationListPage {
       iconColor: AppColors.dsGray2,
       collapsedIconColor: AppColors.dsGray2,
       title: SDSBuildText(
-        LocaleKeys.declareInfo_tk1Declaration.tr,
+        title,
         style: AppTextStyle.font14Bo,
         maxLines: 3,
       ),
       children: addSeparator(
-        children: paths
-            .map(
-              (path) => Row(
-                children: [
-                  const SizedBox(
-                    width: AppDimens.padding40,
-                  ),
-                  Expanded(
-                    child: SDSBuildText(
-                      path.name,
-                      style: AppTextStyle.font14Re,
-                      maxLines: 1,
-                    ),
-                  ),
-                  UtilWidget.sizedBoxWidth16,
-                  InkWell(
-                    onTap: () {
-                      controller.createPdf(
-                        previewDocumentType: PreviewDocumentTypeEnum.tk1,
-                        documentRecordId: path.documentRecordId,
-                        title:
-                            LocaleKeys.declareInfo_participationDeclaration.tr,
-                      );
-                    },
-                    child: SDSImageSvg(Assets.ASSETS_ICONS_IC_EYE_SVG),
-                  ),
-                  UtilWidget.sizedBoxWidth16,
-                ],
-              ).paddingSymmetric(vertical: AppDimens.paddingVerySmall),
-            )
-            .toList(),
+        children: children.toList(),
         spacer: UtilWidget.buildDividerDefault()
             .paddingSymmetric(horizontal: AppDimens.paddingSmallest),
       ),
     ).paddingOnly(bottom: AppDimens.paddingSmall);
+  }
+
+  Widget _buildOnTapTk1({
+    required String name,
+    required VoidCallback onTap,
+  }) {
+    return Row(
+      children: [
+        const SizedBox(width: AppDimens.padding40),
+        Expanded(
+          child: SDSBuildText(
+            name,
+            style: AppTextStyle.font14Re,
+            maxLines: 1,
+          ),
+        ),
+        UtilWidget.sizedBoxWidth16,
+        InkWell(
+          onTap: onTap,
+          child: SDSImageSvg(Assets.ASSETS_ICONS_IC_EYE_SVG),
+        ),
+        UtilWidget.sizedBoxWidth16,
+      ],
+    ).paddingSymmetric(vertical: AppDimens.paddingVerySmall);
   }
 
   Widget _buildDeclarationItem({
